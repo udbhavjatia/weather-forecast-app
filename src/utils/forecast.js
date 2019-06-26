8e5fc97ee6278eba2f336b8c7b8715ad611ab18a
Udbhav <udbhavjatia@gmail.com>
2019-06-26 16:09:44 +0800

    Modified the Forecast Data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees and there is ${body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees and there is ${body.currently.precipProbability}% chance of rain. The temperature range for today is between ${body.daily.data[0].apparentTemperatureLow} degrees and ${body.daily.data[0].apparentTemperatureHigh} degrees.`);
         }
     })
 };
